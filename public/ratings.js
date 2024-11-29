1dd91fe83d53fc0cb353ae867b8bcbb5fadbb3c0
jav1ergp <javiergp23100130@gmail.com>
2024-11-30 00:50:20 +0100

    Practica 5

--- public/ratings.js
+++ public/ratings.js
@@ -1,55 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Iniciando fetch ...");
-  const ele_stars = document.getElementsByClassName("stars"); // todos los elementos de la clase 'stars' que haya en la página
+  
+  function Estrellas(rating, count, ide) {
+    const Total = 5;
+    let enteras = parseInt(rating);
+    let mitad;
+    if (rating % 1 >= 0.5) { // estrellas a la mitad cuando sean >= 0.5
+      mitad = 1;
+    } else {
+      mitad = 0;
+    }
+    let vacias = Total - enteras - mitad; // estrellas vacías
+  
+    let estrellasHTML = "";
+  
+    for (let i = 0; i < enteras; i++) {
+      estrellasHTML += `<i class="fa fa-star" data-_id="${ide}" data-star="${i + 1}"></i>`; // id = id del producto y star = posicion de la estrella
+    }
+  
+    if (mitad) {
+      estrellasHTML += `<i class="fa fa-star-half-o" data-_id="${ide}" data-star="${enteras + 1}"></i>`; // id = id del producto y star = posicion de la estrella
+    }
+  
+    for (let i = 0; i < vacias; i++) {
+      estrellasHTML += `<i class="fa fa-star-o" data-_id="${ide}" data-star="${enteras + mitad + i + 1}"></i>`; // id = id del producto y star = posicion de la estrella
+    }
+  
+    estrellasHTML += `<span class="text-body-secondary"> (${count} votos)</span>`;
+  
+    return estrellasHTML;
+  }
+  
+  function Vota(evt) {
+    const ide = evt.target.dataset._id; // id producto
+    const pun = parseInt(evt.target.dataset.star); // posicion estrella
+  
+    if(ide && pun){
+      const options = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ // posicion de la estrella para calcular el nuevo
+          rate: pun
+        }),
+      };
+      fetch(`/api/ratings/${ide}`, options)
+        .then(res => res.json())
+        .then(res => {
+          console.log('Respuesta de la API:', res);
+          evt.target.parentElement.innerHTML = Estrellas(res.rating.rate, res.rating.count, ide);
+        })
+        .catch(err => console.error('Error:', err));
+    }
+  }
+  
+
+  const ele_stars = document.querySelectorAll('.stars');
 
   for (const ele of ele_stars) {
-    const ide = ele.dataset._id; // _id esta en los atributos del dataset
+    const ide = ele.dataset._id;
 
-    // hacer el fetch, y con lo que devuelva formar el html y ponerlo
     fetch(`/api/ratings/${ide}`)
-      .then((response) => {
-        if (!response.ok) { // response html
-          throw new Error("Error del response html");
-        }
-        return response.json();
-      })
-      .then((data) => { // estrellas
+      .then(res => res.json())
+      .then(data => {
         let rating = data.rating.rate;
-        html_nuevo_con_las_estrellas = Estrellas(rating);
+        let count = data.rating.count;
+        let html_nuevo_con_las_estrellas = Estrellas(rating, count, ide);
         ele.innerHTML = html_nuevo_con_las_estrellas;
       })
-      .catch(() => (ele.innerHTML = "Error al cargar el rating"));
-  }
+      .catch(err => console.error('Error:', err));
+  };
+
+  // un solo event listener para manejar en Vota todos los clics de estrellas
+  document.addEventListener('click', Vota);
 });
-
-function Estrellas(rating) {
-  const Total = 5;
-  let enteras = parseInt(rating);
-  let mitad;
-  if (rating % 1 >= 0.5) { // estrellas a la mitad cuando sean >= 0.5
-    mitad = 1;
-  } else {
-    mitad = 0;
-  }
-  let vacias = Total - enteras - mitad; // estrellas vacías
-
-  let estrellasHTML = "";
-
-  // estrellas enteras
-  for (let i = 0; i < enteras; i++) {
-    estrellasHTML += '<i class="fa fa-star"></i>';
-  }
-
-  // estrella media
-  if (mitad) {
-    estrellasHTML += '<i class="fa fa-star-half-o"></i>';
-  }
-
-  // Agregar estrellas vacías
-  for (let i = 0; i < vacias; i++) {
-    estrellasHTML += '<i class="fa fa-star-o"></i>';
-  }
-
-
-  return estrellasHTML;
-}
--- routes/router_api_rating.js
+++ routes/router_api_rating.js
@@ -8,33 +8,25 @@ router.get('/', async (req, res)=>{
         const desde = parseInt(req.query.desde) || 0;
         const hasta = parseInt(req.query.hasta) || 5;
     
-        if (desde < 0 || hasta <= desde) {
-          return res.status(400).json({ 
-            error: true, 
-            message: "'desde' debe ser >= a 0 y 'hasta' mayor que 'desde'" 
-          });
+        if (desde < 0 || hasta <= desde) { // error parametros 
+            return res.status(400).json({ message: "'desde' debe ser >= a 0 y 'hasta' mayor que 'desde'" });
         }
         const productos = await Productos.find().select("rating").skip(desde).limit(hasta - desde);
-        res.status(200).json(productos);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: true, message: error.message });
+        res.status(200).json(productos); // respuesta
+    } catch (error) { // error server
+        res.status(500).json({ error: true, message: error.message }); 
     }
 });
 
 router.get("/:id", async (req, res) => {
     const ide = req.params.id;
-
     try {
-        const producto = await Productos.findById(ide).select("rating");
-        console.log(producto);
-
-        if (!producto) {
-            return res.status(404).json({ message: "Producto no encontrado" });
+        const producto = await Productos.findById(ide).select("rating"); 
+        if (!producto) {  
+            return res.status(404).json({ message: "Producto no encontrado" }); // error parametros 
         }
-        res.status(200).json(producto);
-    } catch (error) {
-        console.log(error);
+        res.status(200).json(producto); // respuesta
+    } catch (error) { // error server
         res.status(500).json({ error: true, message: error.message });
     }
 });
@@ -43,22 +35,17 @@ router.get("/:id", async (req, res) => {
 router.put('/:id', async (req, res)=>{
     const ide = req.params.id;
     const rate = req.body.rate;
-
     try {
         const { rating } = await Productos.findById(ide).select("rating");
-        console.log('rating:', rating);
         if(!rating) {
             return res.status(404).json({ message: 'Producto no encontrado' });
         }
-
         const new_rate = (rating.rate * rating.count + rate) / (rating.count + 1);
         const new_rating = {rate: new_rate, count: rating.count + 1};
-
-        const producto  = await Productos.findByIdAndUpdate(ide, { rating:new_rating }, { new:true, runValidators:true });
-        console.log('Producto actualizado:', producto);
-        res.json(producto);
-    } catch (error) {
-        console.log(error);
+        
+        const producto  = await Productos.findByIdAndUpdate(ide, { rating:new_rating }, { new:true });
+        res.json(producto); // respuesta
+    } catch (error) { // error server
         res.status(500).json({ error: true, message: error.message });
     }
 });
